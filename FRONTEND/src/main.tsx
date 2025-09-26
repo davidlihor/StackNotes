a11e45d959913eacc7cbca97bd9aa110d59c040f
David <davidlihor.dev@gmail.com>
2025-09-26 12:09:49 +0300

    chore: update .gitignore and README for dotenv file handling; replace sensitive data in docker-compose

--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./main.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
